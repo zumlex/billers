bb8a8c991f60d7568a18b78df97dad2d85bca1e8
Qasim <qahmed@zumlex.com>
2024-02-19 02:15:45 +0000

    Services

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+//  swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
@@ -8,16 +8,9 @@ let package = Package(
             name: "Billers",
             targets: ["Billers"]),
     ],
-    dependencies: [
-        // Add your specific dependencies here
-     //   .package(url: "https://github.com/dependency1.git", from: "1.0.0"),
-      //  .package(url: "https://github.com/dependency2.git", from: "2.0.0"),
-        // Add more dependencies if needed
-    ],
     targets: [
         .target(
-            name: "Billers",
-       //     dependencies: ["dependency1", "dependency2"]),
+            name: "Billers"),
         .testTarget(
             name: "BillersTests",
             dependencies: ["Billers"]),
