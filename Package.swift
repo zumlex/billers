c6eb6e0f6db0949c9173c1b5e7468209644b770c
Qasim <qahmed@zumlex.com>
2024-02-19 02:09:34 +0000

    Services

--- Package.swift
+++ Package.swift
@@ -9,9 +9,6 @@ import PackageDescription
 
 let package = Package(
     name: "Billers",
-    platforms: [
-        .macOS(.v10_15),
-    ],
     products: [
         .library(
             name: "Billers",
@@ -19,14 +16,14 @@ let package = Package(
     ],
     dependencies: [
         // Add your specific dependencies here
-        .package(url: "https://github.com/dependency1.git", from: "1.0.0"),
-        .package(url: "https://github.com/dependency2.git", from: "2.0.0"),
+     //   .package(url: "https://github.com/dependency1.git", from: "1.0.0"),
+      //  .package(url: "https://github.com/dependency2.git", from: "2.0.0"),
         // Add more dependencies if needed
     ],
     targets: [
         .target(
             name: "Billers",
-            dependencies: ["dependency1", "dependency2"]),
+       //     dependencies: ["dependency1", "dependency2"]),
         .testTarget(
             name: "BillersTests",
             dependencies: ["Billers"]),
