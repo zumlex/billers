88407d0462c7a5253c17da0c99f7ee04778201d2
Qasim <qahmed@zumlex.com>
2024-02-19 01:07:16 +0000

    Services

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "Billers",
+    platforms: [
+        .macOS(.v10_15),
+    ],
+    products: [
+        .library(name: "Billers", targets: ["Billers"]),
+    ],
+    targets: [
+        .target(name: "Billers"),
+    ]
+)
