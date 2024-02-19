24c947c856932f6ca59290807158657f75e728d2
Qasim <qahmed@zumlex.com>
2024-02-19 01:00:30 +0000

    Services

---
+++ Billers Integrated/Billers_Integrated.h
@@ -0,0 +1,18 @@
+//
+//  Billers_Integrated.h
+//  Billers Integrated
+//
+//  Created by okan on 19.02.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Billers_Integrated.
+FOUNDATION_EXPORT double Billers_IntegratedVersionNumber;
+
+//! Project version string for Billers_Integrated.
+FOUNDATION_EXPORT const unsigned char Billers_IntegratedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Billers_Integrated/PublicHeader.h>
+
+
--- sources/billers/get provides.swift
+++
@@ -1,8 +0,0 @@
-//
-//  get provides.swift
-//  intergrated-billers
-//
-//  Created by okan on 17.02.2024.
-//
-
-import Foundation
